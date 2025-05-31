6cc6b1726363cabc5b20803eeb21246c75999fb4
tsui-ethan <ethan.sn.tsui@gmail.com>
2025-05-31 11:25:50 +0000

    Minimal Vercel API route and ready for deployment

--- api/hello.ts
+++ api/hello.ts
@@ -1,3 +1,3 @@
 export default function handler(req, res) {
-  res.status(200).json({ message: "Hello from API!" });
+  res.status(200).json({ message: "This is a Vercel serverless function!" });
 }
